e86580580ce4e71718573a3fd91be13043b489dc
Tapu Saha <tapu.saha98@gmail.com>
2025-05-10 13:33:05 +0600

    chore (init): initialised react native app

---
+++ app/screens/home/HomeScreen.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View>
+    <View className="w-15 h-80 border-2 border-red-100 m-3" />
+    <Text className='text-xl'>Hi</Text>
+    <Text className='text-xl'>Hi</Text>
+    <Text className='text-xl'>Hi</Text>
+    <Text className='text-xl'>Hi</Text>
+    <Text className='text-xl'>Hi</Text>
+    <Text className='text-xl'>Hi</Text>
+  </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
