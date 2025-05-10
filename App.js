d7f930589fff2611eeb70e67b5031127fdcaa95f
Tapu Saha <tapu.saha98@gmail.com>
2025-05-10 14:45:12 +0600

    feat (homescreen): homescreen UI Design

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { View } from 'react-native';
-import HomeScreen from './app/screens/home/HomeScreen';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import "./global.css";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './app/screens/home/HomeScreen';
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: '#FFF',
+    },
+  };
   return (
-    <View >
-      <HomeScreen />
-    </View>
+    <NavigationContainer theme={MyTheme}>
+      <Stack.Navigator>
+      <Stack.Screen name="Home" options={{
+        title: "Marchant details"
+      }} component={HomeScreen} />
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- app/screens/home/HomeScreen.jsx
+++ app/screens/home/HomeScreen.jsx
@@ -1,20 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from "react";
+import {
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Feather from "@expo/vector-icons/Feather";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 const HomeScreen = () => {
   return (
-    <View>
-    <View className="w-15 h-80 border-2 border-red-100 m-3" />
-    <Text className='text-xl'>Hi</Text>
-    <Text className='text-xl'>Hi</Text>
-    <Text className='text-xl'>Hi</Text>
-    <Text className='text-xl'>Hi</Text>
-    <Text className='text-xl'>Hi</Text>
-    <Text className='text-xl'>Hi</Text>
-  </View>
-  )
-}
+    <SafeAreaView className={styles.container}>
+      <View className={styles.shadowContainer}>
+        <View className={styles.searchBox}>
+          <Feather name="search" size={20} color="gray" />
+          <TextInput
+            className={styles.input}
+            placeholder="Merchant name or number"
+          />
+        </View>
 
-export default HomeScreen
+        <TouchableOpacity className={styles.scanButton}>
+          <Text className={styles.scanButtonText}>Scan QR Code</Text>
+          <MaterialIcons name="qr-code-scanner" size={24} color="#16a34a" />
+        </TouchableOpacity>
+      </View>
 
-const styles = StyleSheet.create({})
+      <TouchableOpacity className={styles.nextButton}>
+        <Text className={styles.nextButtonText}>Next</Text>
+        <AntDesign name="arrowright" size={20} color="white" />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = {
+  container: `flex-1 mx-4 my-2 justify-between`,
+  shadowContainer: `shadow-lg`,
+  searchBox: `border border-gray-400 rounded-sm px-1 py-2 flex-row gap-2 items-center`,
+  input: `text-xl flex-1`,
+  scanButton: `border border-green-600 rounded-sm mt-5 py-2 flex-row items-center justify-center gap-2`,
+  scanButtonText: `text-green-600 text-xl`,
+  nextButton: `rounded-lg mt-2 py-4 bg-green-600 flex-row items-center justify-center gap-2`,
+  nextButtonText: `text-white text-center text-lg`,
+};
