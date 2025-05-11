102a7b356c1bb52db5dc4952839863bf59a27a5d
Tapu Saha <tapu.saha98@gmail.com>
2025-05-12 03:32:26 +0600

    spacing issue fix

--- app/screens/MarchantDetails/MarchantDetails.jsx
+++ app/screens/MarchantDetails/MarchantDetails.jsx
@@ -8,8 +8,9 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-  View
+  View,
 } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const MarchantDetails = () => {
   const navigation = useNavigation();
@@ -18,6 +19,10 @@ const MarchantDetails = () => {
     navigation.navigate("PaymentOptions");
   };
 
+  const insets = useSafeAreaInsets();
+
+  console.log("insets", insets);
+
   return (
     <SafeAreaView className="flex-1 mx-4 my-2 mt-5 justify-between">
       <View>
@@ -25,18 +30,26 @@ const MarchantDetails = () => {
           <Feather name="search" size={18} color="gray" />
           <TextInput
             placeholder="Marchant name or number"
-            placeholderTextColor={'gray'}
+            placeholderTextColor={"gray"}
             className="flex-1 text-xl font-medium py-3 ml-2"
           />
         </View>
 
         <TouchableOpacity className="border border-green-600 rounded-xl mt-7 py-3 flex-row items-center justify-center gap-1">
-          <Text className="text-green-600 text-xl font-medium">Scan QR Code</Text>
+          <Text className="text-green-600 text-xl font-medium">
+            Scan QR Code
+          </Text>
           <MaterialIcons name="qr-code-scanner" size={24} color="#16a34a" />
         </TouchableOpacity>
       </View>
 
-      <TouchableOpacity  onPress={handleStartButton} className="rounded-lg mt-2 py-4 bg-green-600 flex-row items-center justify-center gap-1">
+      <TouchableOpacity
+        onPress={handleStartButton}
+        style={{
+          marginBottom: insets.bottom,
+        }}
+        className="rounded-lg mt-2 py-4 bg-green-600 flex-row items-center justify-center gap-1"
+      >
         <Text className="text-white text-center text-xl">Next</Text>
         <AntDesign name="arrowright" size={20} color="white" />
       </TouchableOpacity>
--- app/screens/PaymentOptions/PaymentOptions.jsx
+++ app/screens/PaymentOptions/PaymentOptions.jsx
@@ -2,11 +2,11 @@ import AntDesign from "@expo/vector-icons/AntDesign";
 import React from "react";
 import {
   Image,
-  SafeAreaView,
   Text,
   TouchableOpacity,
   View
 } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const PaymentOptions = () => {
   const itemList = [
@@ -16,25 +16,31 @@ const PaymentOptions = () => {
     { image: require("../../../assets/images/rocket.png") },
     { image: require("../../../assets/images/card.png") },
   ];
+  const insets = useSafeAreaInsets();
   return (
-      <View className="flex-1 px-4 py-2 justify-between">
-        <View className="flex-row flex-wrap gap-3 ml-3">
-          {itemList.map((item, index) => (
-            <Image
-              key={index}
-              source={item.image}
-              className="w-[22%] h-[90px] rounded-xl mb-2 rounded-lg border border-gray-200"
-            />
-          ))}
-        </View>
-
-        <TouchableOpacity className="border border-green-600 rounded-lg py-4 mt-3 mb-8 flex-row justify-center items-center space-x-2">
-          <Text className="text-green-600 text-base text-center">
-            Scan QR Code
-          </Text>
-          <AntDesign name="arrowright" size={20} color="green" />
-        </TouchableOpacity>
+    <View className="flex-1 px-4 py-2 justify-between">
+      <View className="flex-row flex-wrap gap-3 ml-3">
+        {itemList.map((item, index) => (
+          <Image
+            key={index}
+            source={item.image}
+            className="w-[22%] h-[80px] rounded-xl mb-2 rounded-lg border border-gray-200"
+          />
+        ))}
       </View>
+
+      <TouchableOpacity
+        style={{
+          marginBottom: insets.bottom,
+        }}
+        className="border border-green-600 rounded-lg py-4 mt-3 mb-8 flex-row justify-center items-center space-x-2"
+      >
+        <Text className="text-green-600 text-base text-center">
+          Scan QR Code
+        </Text>
+        <AntDesign name="arrowright" size={20} color="green" />
+      </TouchableOpacity>
+    </View>
   );
 };
 
